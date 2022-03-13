030c4a30ca1e9dca85cf369a16d8973d4d0f731b
Vaibhav <singhvaibhav557hr@gmail.com>
2022-03-13 13:19:11 +0530

    first

---
+++ src/constants/essential.js
@@ -0,0 +1,475 @@
+export const essential = [
+  {
+    text: "Documents (Original & Attested Photocopy)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Admit Card (if any) for which you are joining the college",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Acceptance Letter",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Offer Letter",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Class XII Mark sheet",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Class XII Passing Certificate",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Address Proof",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Aadhar Card",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Plenty of Passport Sized Photographs ",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Cotton",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "ORS",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Paracetamol/Crocin",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Amoxicillin",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Antiseptic spray",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Dettol",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Crepe Bandage",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Dabur Pudin Hara ",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Shirt – Full & Half",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "T-Shirts",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Jeans",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Shorts",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Undergarments",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Formal Trouser - 1 (at least)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Cotton towel",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Socks",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Kurta & Pyjama (for events)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Tracksuit – 1",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Blanket",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Bedsheet with pillow cover – 2 atleast",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Raincoat / Umbrella",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Spectacles (If you have them)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Hangers 2 -3",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Lock and key – 2 pairs",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Wallet/purse",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Belt",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "God Idol – If you plan to pray",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Nail-Cutter",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Slippers (Bathroom)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Formal Black Shoes",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Sandals",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Black - Blue Pens",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Glue",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Stapler",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Scissor",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Metallic Scale",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Highlighter",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Tape",
+
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Fevicol & fevistick",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Paper Pins",
+
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Pencil Box",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Pen Stand",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Mobile Phone with Charger",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Scientific Calculator",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Laptop",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Toothbrush",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Toothpaste",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Tongue Cleaner",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Soap Case (big one preferred)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Dettol Handwash",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Tiger Balm for Cold / Vicks Vapo Rub",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Iodex (for Muscular pain)",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "A box to keep medicines",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Mattress",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Pillow",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Bucket and Mug",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Quilt ",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Half sweaters",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Full Sweaters",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Jackets/Coats",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Any warm innerwear",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  { text: "Namkeem", isDone: false, id: Math.random() * 1000 },
+  {
+    text: "Dabar Chaywanprash – Saves from cold/cough",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Fruits",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Biscuits",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Clips to attatch clothes to rope",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Rope to hang clothes",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Wrist Watch",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Sports Shoes – Running",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Table cloth ",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Cap",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Some Rough Books",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Liquid Polish",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Torch ",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Camera",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Pen Drive",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Mouthwash",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Relispray",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Soframicin–Antibacterial Cream",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Cycle",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Swimming costume and cap",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: " Badminton, TT, Squash etc rackets",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Cricket Gear",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Football, football Socks and boots",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Gym equipment",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+  {
+    text: "Midnight Munching Snacks - Makhane  , Peanuts",
+    isDone: false,
+    id: Math.random() * 1000,
+  },
+];
